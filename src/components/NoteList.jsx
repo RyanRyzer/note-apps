4fe05fcd1eac7312bc7a7d065324041f9a362b20
Ryan Robby <147597256+RyanRyzer@users.noreply.github.com>
2024-12-22 11:54:14 +0700

    Update NoteList.jsx

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import NoteItem from "./NoteItem";
 
 function NoteList({ notes, onDelete, onArchive, onUnarchive }) {
+    if (notes.length === 0) {
+        return <p>{onArchive ? "Tidak ada catatan yang aktif" : "Tidak ada catatan yang diarsipkan"}</p>;
+    }
+
     return (
         <div className="note-list">
             {
                 notes.map((note) => (
                     <NoteItem
-                    key={note.id}
-                    id={note.id}
-                    onDelete={onDelete}
-                    onArchive={onArchive}
-                    onUnarchive={onUnarchive}
-                    {...note} />
+                        key={note.id}
+                        id={note.id}
+                        onDelete={onDelete}
+                        onArchive={onArchive}
+                        onUnarchive={onUnarchive}
+                        {...note} />
                 ))
             }
         </div>
